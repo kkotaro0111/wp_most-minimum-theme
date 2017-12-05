4dfbe4bf57498966b1cd51fe1738c555f782d614
Kotaro Kawashima <utageyanagi+kkgithub@gmail.com>
2017-12-05 22:07:28 +0900

    Add: activate custome menu function.

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ require_once('functions/thumbnail.php'); // サムネイル関連
 require_once('functions/svg.php'); // SVGコードを直接埋め込むためのメソッド/ショートコード
 require_once('functions/auto_post_slug.php'); // 投稿記事名に2バイト文字を許可しないためのhook
 
+# require_once('functions/activate_menu.php'); // カスタムメニューを使えるようにする
 # require_once('functions/widget.php'); // ウィジェットメニューを追加する
 # require_once('functions/rewrite_rules.php'); // URL Rewriteを設定する
 # require_once('functions/change_posts_per_page.php'); // 投稿タイプごとの表示件数を変更する
---
+++ functions/activate_menu.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kawashimakoutarou
+ * Date: 2017/12/05
+ * Time: 22:06
+ */
+
+add_theme_support('menus');
